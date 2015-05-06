14172e66582d2c4c147289cbfa2a063a8311ac16
Derek Morris <dmorris321@gmail.com>
2015-05-06 03:15:31 -0400

    Add AntonAderum's parser. It fails a test case, so don't use it yet.

---
+++ KVLib/KeyValues/AderumParser.cs
@@ -0,0 +1,209 @@
+﻿using Sprache;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KVLib.KeyValues
+{
+    public class AderumParser : IKVParser
+    {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public AderumParser()
+        {
+
+        }
+        #region TextModel
+        static Parser<char> DisallowedKeyChar = Sprache.Parse.Char('}').XOr(Sprache.Parse.Char('{'));
+
+        static Parser<string> Comment = Sprache.Parse.EndOfLineComment("//");
+
+        static Parser<IEnumerable<string>> AllComments =
+            from first in Comment.Token().Many()
+            select first;
+
+
+        static Parser<string> UnquotedKVString =
+            from rest in Sprache.Parse.AnyChar.Except(DisallowedKeyChar).Until(Sprache.Parse.WhiteSpace.AtLeastOnce())
+            select new string(rest.ToArray());
+
+        static Parser<string> QuotedKVString =
+            from openquote in Sprache.Parse.Char('"')
+            from rest in Sprache.Parse.AnyChar.Except(DisallowedKeyChar).Until(Sprache.Parse.Char('"'))
+            select new string(rest.ToArray());
+
+        static Parser<string> KVString = QuotedKVString.Or(UnquotedKVString);
+
+
+        static Parser<KeyValue> SubKey(string key)
+        {
+            return from open in Sprache.Parse.Char('{').Token().Named("Start of Block")
+                   from value in Sprache.Parse.Ref(() => Item).Many()
+                   from close in Sprache.Parse.Char('}').Token().Named("End of Block")
+                   select (new KeyValue(key, value));
+        }
+
+        static Parser<KeyValue> Value(string key)
+        {
+            return from Value in KVString.Token().Named("Content Value")
+                   select (new KeyValue(key) { Value = Value });
+
+        }
+
+        static Parser<KeyValue> Item =
+            from c1 in AllComments.Token().Optional()
+            from Key in KVString.Named("Key").Token()
+            from c in AllComments.Token().Optional()
+            from nodes in SubKey(Key).Token().XOr(Value(Key).Named("Value"))
+            from c2 in AllComments.Token().Optional()
+            select nodes;
+
+
+
+
+        static Parser<KeyValue> Document =
+            from leading in AllComments.Token().Optional()
+            from node in Item
+            select node;
+
+        #endregion
+
+        #region ManualParse 
+
+        enum parseEnum {foundFirstKey,foundSecondKey,foundFirstValue,foundSecondvalue, nil};
+        static parseEnum parseState = parseEnum.nil;
+
+        public static KeyValue ParseKeyValueFile(string path)
+        {
+            string[] lines = System.IO.File.ReadAllLines("SampleKeyValues/" + path);
+            KeyValue kv = new KeyValue(""); 
+            foreach(string str in lines)
+            {
+                if(str.StartsWith("{") || str.StartsWith("//")){ continue; }
+                if (str.Contains("}")) 
+                { 
+                    kv = kv.Parent ?? kv; //we reached the end of a parent, go up one level 
+                    continue;
+                }
+
+                string key = null;
+                string value = null; 
+                for (int i = 0; i < str.Length;i++ )
+                {
+                    if (str.ElementAt(i) == '/' && str.ElementAt(i + 1) == '/') 
+                    {
+                        break;
+                    }
+                    switch(parseState)
+                    {
+                        case parseEnum.nil:
+                            if(str.ElementAt(i) == '"')
+                            {
+                                parseState = parseEnum.foundFirstKey;
+                                key = "";
+                                continue;
+                            }
+                            break;
+                        case parseEnum.foundFirstKey:
+                            if(str.ElementAt(i) != '"')
+                            {
+                                key = key + str.ElementAt(i).ToString();
+                            }
+                            else
+                            {
+                                parseState = parseEnum.foundSecondKey;
+                                continue;
+                            }
+                            break;
+                        case parseEnum.foundSecondKey:
+                            if(str.ElementAt(i) == '"')
+                            {
+                                parseState = parseEnum.foundFirstValue;
+                                value = "";
+                                continue;
+                            }
+                            break;
+                        case parseEnum.foundFirstValue:
+                            if(str.ElementAt(i) != '"')
+                            {
+                                value = value + str.ElementAt(i).ToString();
+                            }
+                            else
+                            {
+                                parseState = parseEnum.foundSecondvalue;
+                                continue;
+                            }
+                            break;
+                        case parseEnum.foundSecondvalue:
+                            //
+                            break;
+                    }
+
+
+                }
+                parseState = parseEnum.nil;
+
+                if (key == null && value == null) { continue; }
+
+                if(value == null)
+                {
+                    if (kv.Key == "")
+                    {
+                        kv = new KeyValue(key);
+                    }
+                    else
+                    {
+                        KeyValue tmpKv = new KeyValue(key);
+                        kv.AddChild(tmpKv);
+                        kv = tmpKv; //we found a parent, set kv to be the parent we found
+                    }
+                }
+                else
+                {
+                    KeyValue tmpKv = new KeyValue(key);
+                    tmpKv.Value = value;
+                    kv.AddChild(tmpKv);
+                }
+                
+            }
+
+            return kv;
+        }
+        #endregion
+
+        /// <summary>
+        /// Reads a line of text and returns the first Root key in the text
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public KeyValue Parse(string text)
+        {
+
+            try
+            {
+                KeyValue i = Document.Parse(text);
+                return i;
+            }
+            catch (Sprache.ParseException e)
+            {
+                throw new KeyValueParsingException(e.Message, e);
+            }
+
+        }
+        /// <summary>
+        /// Reads a blob of text and returns an array of all root keys in the text
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public KeyValue[] ParseAll(string text)
+        {
+            IEnumerable<KeyValue> i = Document.Many().Parse(text);
+            return i.ToArray();
+        }
+
+
+    }
+}
---
+++ Tests/AderumParserTester.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KVLib;
+using KVLib.KeyValues;
+using System.Linq;
+
+namespace KVLib
+{
+    [TestClass]
+    public class AderumParserTester
+    {
+        [TestMethod]
+        public void ASample1()
+        {
+            KeyValue kv = KVTestHelper.Sample1(new AderumParser());
+
+            Assert.AreEqual(kv.Key, "SomeTest");
+            Assert.IsNotNull(kv["Key"]);
+            Assert.AreEqual(kv["Key"].GetString(), "Value");
+
+        }
+
+        [TestMethod]
+        public void ASample2()
+        {
+            KeyValue kv = KVTestHelper.Sample2(new AderumParser());
+
+            Assert.AreEqual(kv.Key, "Sample2");
+            Assert.IsNotNull(kv["Key"]);
+            Assert.AreEqual(kv["Key"].GetString(), "Value");
+
+        }
+
+        [TestMethod]
+        public void ASample3()
+        {
+            KeyValue kv = KVTestHelper.Sample3(new AderumParser());
+
+            Assert.AreEqual(kv.Key, "Sample3");
+            Assert.IsNotNull(kv["Key"]);
+            Assert.AreEqual(kv["Key"].GetString(), "Value");
+            Assert.IsNotNull(kv["Subkey"]);
+            Assert.AreEqual(kv["Subkey"].Key, "Subkey");
+            Assert.IsNotNull(kv["Subkey"]["Key"]);
+            Assert.AreEqual(kv["Subkey"]["Key"].GetString(), "Value");
+        }
+
+        [TestMethod]
+        public void ASample4()
+        {
+            /* This sample was taken from the Dota 2 project sample for Holdout
+             * It had an issue because on line 6 there is a key/value pair with a comment at the end, but not whitespace 
+             * betwen the comment and the end of the value.  
+             */
+            KeyValue kv = KVTestHelper.Sample4(new AderumParser());
+
+            Assert.AreEqual(kv.Key, "npc_dota_holdout_tower");
+
+            Assert.IsNotNull(kv["BaseClass"]);
+
+            Assert.IsNotNull(kv["Model"]);
+            Assert.AreEqual(kv["Model"].GetString(), "models/props_structures/tower_good.vmdl");
+
+            Assert.IsNotNull(kv["SoundSet"]);
+            Assert.AreEqual(kv["SoundSet"].GetString(), "Tower.Water");
+        }
+
+        [TestMethod]
+        public void ASample5()
+        {
+            //In this sample on line 128 there is a KeyValue pair without a space
+            //between the key and the value.  
+
+            //Check that value and make sure it returns 50.
+
+            KeyValue kv = KVTestHelper.Sample5(new AderumParser());
+
+            Assert.AreEqual("DOTAUnits", kv.Key);
+
+            KeyValue SpecialKey = kv["npc_dota_creature_berserk_zombie"]["Creature"]["DefensiveAbilities"]["Ability1"];
+            Assert.AreEqual(50, SpecialKey["UseAtHealthPercent"].GetInt());
+        }
+
+        [TestMethod]
+        public void ASample6()
+        {
+            KeyValue kv = KVTestHelper.Sample6(new AderumParser());
+
+            Assert.AreEqual("DOTAAbilities", kv.Key);
+        }
+
+        [TestMethod]
+        public void AIssue1()
+        {
+            KeyValue kv = KVTestHelper.Issue1(new AderumParser());
+            Assert.AreEqual(kv.Key, "Issue1Test");
+            Assert.IsNotNull(kv["Test"]);
+
+            Assert.AreEqual(kv["Test"].GetString(), "5");
+            Assert.AreEqual(kv["Test"].GetInt(), 5);
+
+            Assert.IsNotNull(kv["Something"]);
+            Assert.AreEqual(kv["Something"].GetString(), "asdf");
+
+            Assert.IsNotNull(kv["Subkey"]);
+            Assert.AreEqual(kv["Subkey"].Key, "Subkey");
+            Assert.IsNotNull(kv["Subkey"]["Key"]);
+            Assert.AreEqual(kv["Subkey"]["Key"].GetString(), "Value");
+
+
+        }
+
+        [TestMethod]
+        public void AIssue3()
+        {
+            KeyValue kv = KVTestHelper.Issue3(new AderumParser());
+            Assert.AreEqual(kv.Key, "Issue3Test");
+
+            Assert.IsNotNull(kv["Test"]);
+            Assert.AreEqual(kv["Test"].GetString(), "Value");
+        }
+
+        [TestMethod]
+        public void ASample7()
+        {
+            KeyValue kv = KVTestHelper.Sample7(new AderumParser());
+
+            Assert.AreEqual(kv.Key, "DOTAAbilities");
+            Assert.IsNotNull(kv["ninja_dash_fire"]);
+            Assert.IsNotNull(kv["ninja_dash_fire"]["precache"]);
+            Assert.AreEqual(kv["ninja_dash_fire"]["precache"].Children.Count(), 0);
+        }
+    }
+}

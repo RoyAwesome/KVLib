a28800d633e77abee413fbc487ff5ec75bdd0cef
RoyAwesome <kylegar@kylegar.net>
2015-05-12 02:30:28 -0700

    Removed Sprache and the Sprache Parser

--- KVLib/KVParser.cs
+++ KVLib/KVParser.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Sprache;
 using System.Text.RegularExpressions;
 using KVLib.KeyValues;
 
